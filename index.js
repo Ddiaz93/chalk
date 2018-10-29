464dc717e9ff92ff7e27def494c23725871e93e0
Daniel Diaz <dd0871421@otc.edu>
2018-10-29 15:18:13 -0500

    home work quotes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const chalkAnimation  = require('chalk-animation');
 
-chalkAnimation.rainbow('hellow world');
+//Daniel Diaz
+//Packages Node.js assignment Chalk and animation
+
+const chalkAnimation  = require('chalk-animation');
+const chalk = require('chalk');
+const log = console.log;
+
+    log(chalk.blue('“It is impossible to escape the impression that people commonly use false standards of measurement — that they seek power, success and wealth for themselves and admire them in others, and that they underestimate what is of true value in life" '));
+    log(chalk.green('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t -Sigmund Freud.'))
+    log(chalk.bgRed.bold('“It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened to things.”'));
+    chalkAnimation.rainbow(' \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t -Leonardo Da Vinci ');
+
+
+
+
+
